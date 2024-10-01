b73561b9ba58a9870a4d62b64cf2fee2183a65b0
Hiroki120301 <hiroki20011203@icloud.com>
2024-10-01 19:32:52 -0400

    wip

--- assign1/lb.py
+++ assign1/lb.py
@@ -1,8 +1,9 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.lib.packet import packet
+from ryu.lib.packet import packet, ethernet
+from ryu.lib.packet import lldp, ether_types
 from ryu.ofproto import ofproto_v1_3
 
 import json
@@ -35,17 +36,32 @@ class LoadBalancer(app_manager.RyuApp):
         self.service_mac = self.config['service_mac']
 
         # actual ips of red and blue servers
-        self.h5_ip = self.config['service_ips']['blue'][0]
-        self.h6_ip = self.config['service_ips']['blue'][1]
-        self.h7_ip = self.config['service_ips']['red'][0]
-        self.h8_ip = self.config['service_ips']['red'][1]
-
+        self.h5_ip = self.config['server_ips']['blue'][0]
+        self.h6_ip = self.config['server_ips']['blue'][1]
+        self.h7_ip = self.config['server_ips']['red'][0]
+        self.h8_ip = self.config['server_ips']['red'][1]
         self.ip_to_output_port = dict()
         self.ip_to_mac = dict()
+        self.client_ip_to_mac = dict()
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install the table-miss flow entry.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow_entry(datapath, 0, match, actions)
 
     def send_arp_requests(self, dp, src, dst, msg):
         # handle load balancing
+        ofproto = dp.ofproto
+        parser = dp.ofproto_parser
         if dst == self.blue_service_ip:
+            print(f'    This request is BLUE')
             if len(self.server_to_client[self.h5_ip]) < len(self.server_to_client[self.h6_ip]):
                 self.server_to_client[self.h5_ip].append(src)
                 self.client_to_blue_server[src] = self.h5_ip
@@ -67,68 +83,84 @@ class LoadBalancer(app_manager.RyuApp):
             out_port = self.ip_to_output_port[dst_ip]
         else:
             # broadcast the msg if out port is unknown
+            print('    IT WILL BE BROADCASTED')
             out_port = ofproto.OFPP_FLOOD
+        print(f'     Destination ip is {dst_ip} and output port is {out_port}')
 
         # modify the destination ip to actual ip of the corresponding server
-        actions = [
-            parser.OFPActionOutput(out_port),
-            parser.OFPActionSetField(arp_tpa=dst_ip)
-        ]
-
         # update the MAC address as well if known
+        actions = [
+            parser.OFPActionSetField(arp_tpa=dst_ip),
+        ]
         if dst_ip in self.ip_to_mac.keys():
             actions.append(parser.OFPActionSetField(
                 arp_tha=self.ip_to_mac[dst_ip]))
+        actions.append(parser.OFPActionOutput(out_port))
 
-        # construct out packet
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=msg.match['in_port'], actions=actions, data=msg.data)
-        datapath.send_msg(out)
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+        else:
+            data = None
 
-    def send_proxied_arp_response(self, src_mac, src_ip, msg):
+        print(f'destination is {dst_ip} and {out_port}')
+
+        out = parser.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id,
+                                  in_port=msg.match['in_port'], actions=actions, data=data)
+        dp.send_msg(out)
+
+    def send_proxied_arp_response(self, dp, src_mac, src_ip, dst_ip, msg):
+        print('ARP RESPONSE')
         self.ip_to_mac[src_ip] = src_mac
         self.ip_to_output_port[src_ip] = msg.match['in_port']
+        parser = dp.ofproto_parser
 
-        if src_ip == self.h5_ip or src_ip == self.h6.ip:
+        if src_ip == self.h5_ip or src_ip == self.h6_ip:
             src = self.blue_service_ip
         elif src_ip == self.h7_ip or src_ip == self.h8_ip:
             src = self.red_service_ip
 
         actions = [
-            parser.OFPActionSetField(arp_tpa=src),
-            parser.OFPActionSetField(arp_tha=self.service_mac)
+            parser.OFPActionSetField(arp_spa=src),
+            parser.OFPActionSetField(arp_sha=self.service_mac),
+            parser.OFPActionOutput(self.client_ip_to_mac[dst_ip])
+
         ]
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+        out = parser.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id,
                                   in_port=msg.match['in_port'], actions=actions, data=msg.data)
-        datapath.send_msg(out)
+        dp.send_msg(out)
 
     def send_ip_response(self, dp, src_mac, src_ip, msg):
         self.ip_to_mac[src_ip] = src_mac
         self.ip_to_output_port[src_ip] = msg.match['in_port']
-
-        if src_ip == self.h5_ip or src_ip == self.h6.ip:
+        parser = dp.ofproto_parser
+        if src_ip == self.h5_ip or src_ip == self.h6_ip:
             src = self.blue_service_ip
         elif src_ip == self.h7_ip or src_ip == self.h8_ip:
             src = self.red_service_ip
 
         actions = [
-            parser.OFPActionSetField(ipv4_dst=src),
+            parser.OFPActionSetField(ipv4_src=src),
             parser.OFPActionSetField(eth_src=self.service_mac)
         ]
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+        out = parser.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id,
                                   in_port=msg.match['in_port'], actions=actions, data=msg.data)
-        datapath.send_msg(out)
+        dp.send_msg(out)
 
     def send_proxied_arp_request(self, dp, src, dst, msg):
+        ofproto = dp.ofproto
+        parser = dp.ofproto_parser
         if dst == self.blue_service_ip:
             dst_ip = self.client_to_blue_server[src]
         else:
             dst_ip = self.client_to_red_server[src]
 
-        out_port = self.ip_to_output_port[dst_ip]
+        if dst_ip in self.ip_to_output_port.keys():
+            out_port = self.ip_to_output_port[dst_ip]
+        else:
+            # broadcast the msg if out port is unknown
+            out_port = ofproto.OFPP_FLOOD
         # modify the destination ip to actual ip of the corresponding server
         actions = [
-            parser.OFPActionOutput(out_port),
             parser.OFPActionSetField(arp_tpa=dst_ip)
         ]
 
@@ -137,11 +169,14 @@ class LoadBalancer(app_manager.RyuApp):
             actions.append(parser.OFPActionSetField(
                 arp_tha=self.ip_to_mac[dst_ip]))
 
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+        actions.append(parser.OFPActionOutput(out_port))
+
+        out = parser.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id,
                                   in_port=msg.match['in_port'], actions=actions, data=msg.data)
-        datapath.send_msg(out)
+        dp.send_msg(out)
 
     def send_ip_request(dp, src_mac, dst_mac, src_ip, dst_ip, msg):
+        parser = dp.ofproto_parser
         if dst_ip == self.blue_service_ip:
             if src_ip in self.client_to_blue_server.keys():
                 dst_server_ip = self.client_to_blue_server[src_ip]
@@ -209,38 +244,46 @@ class LoadBalancer(app_manager.RyuApp):
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
+        print('Arrived')
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
+        print(in_port)
 
         eth, pkt_type, pkt_data = ethernet.ethernet.parser(msg.data)
 
         src_mac, dst_mac = eth.src, eth.dst
         dp_id = datapath.id
-
         if eth.ethertype == ether_types.ETH_TYPE_ARP:
             arp_pkt, _, _ = pkt_type.parser(pkt_data)
             src_ip, dst_ip = arp_pkt.src_ip, arp_pkt.dst_ip
             if src_ip in self.server_to_client.keys():
+                print(
+                    f'  This is an ARP Response. src ip is {src_ip} and the dst ip is {dst_ip}')
                 self.send_proxied_arp_response(
-                    dp=datapath, src_mac=src_mac, src_ip=src_ip)
-            elif (src_ip in client_to_blue_server.keys() and dst_ip == self.blue_service_ip) or (src_ip in client_to_red_server.keys() and dst_ip == self.red_service_ip):
+                    dp=datapath, src_mac=src_mac, src_ip=src_ip, dst_ip=dst_ip, msg=msg)
+            elif (src_ip in self.client_to_blue_server.keys() and dst_ip == self.blue_service_ip) or (src_ip in self.client_to_red_server.keys() and dst_ip == self.red_service_ip):
+                self.client_ip_to_mac[src_ip] = in_port
                 self.send_proxied_arp_request(
                     dp=datapath, src=src_ip, dst=dst_ip, msg=msg)
             else:
+                print(
+                    f'  This is an ARP Reuqest. src ip is {src_ip} and the dst ip is {dst_ip}')
+                self.client_ip_to_mac[src_ip] = in_port
                 self.send_arp_requests(
                     dp=datapath, src=src_ip, dst=dst_ip, msg=msg)
         elif eth.ethertype == ether_types.ETH_TYPE_IP:
             ipv4_pkt, _, _ = pkt_type.parser(pkt_data)
-            src_ip, dst_ip = ipv4_pkt.src, ipv4_pkt.ds
+            src_ip, dst_ip = ipv4_pkt.src, ipv4_pkt.dst
             if src_ip in self.server_to_client.keys():
                 self.send_ip_response(
                     dp=datapath, src_mac=src_mac, src_ip=src_ip)
             else:
+                self.client_ip_to_mac[src_ip] = in_port
                 self.send_ip_request(
-                    dp=datapath, src=src, dst=dst, src_ip=src_ip, dst_ip=dst_ip, msg=msg)
+                    dp=datapath, src_mac=src_mac, dst_mac=dst_mac, src_ip=src_ip, dst_ip=dst_ip, msg=msg)
 
     @set_ev_cls(ofp_event.EventOFPFlowRemoved, MAIN_DISPATCHER)
     def flow_removed_handler(self, ev):
